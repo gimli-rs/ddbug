80f633bebf389be8ecd90ed1b56029f06a61a59d
Philip Craig <philipjcraig@gmail.com>
2018-03-14 18:18:41 +1000

    Split out a text printing module

---
+++ src/print/text.rs
@@ -0,0 +1,96 @@
+use std::io::Write;
+
+use {Options, Result};
+use super::{DiffPrefix, Printer};
+
+pub(crate) struct TextPrinter<'w> {
+    w: &'w mut Write,
+    indent: usize,
+    prefix: DiffPrefix,
+    inline_depth: usize,
+}
+
+impl<'w> TextPrinter<'w> {
+    pub fn new(w: &'w mut Write, options: &Options) -> Self {
+        TextPrinter {
+            w,
+            indent: 0,
+            prefix: DiffPrefix::None,
+            inline_depth: options.inline_depth,
+        }
+    }
+}
+
+impl<'w> Printer for TextPrinter<'w> {
+    /// Calls `f` to write to a temporary buffer, then only
+    /// outputs that buffer if `f` return true.
+    fn buffer(&mut self, f: &mut FnMut(&mut Printer) -> Result<bool>) -> Result<bool> {
+        let mut buf = Vec::new();
+        let ret = {
+            let mut p = TextPrinter {
+                w: &mut buf,
+                indent: self.indent,
+                prefix: self.prefix,
+                inline_depth: self.inline_depth,
+            };
+            f(&mut p)?
+        };
+        let ret = ret && !buf.is_empty();
+        if ret {
+            self.w.write_all(&*buf)?;
+        }
+        Ok(ret)
+    }
+
+    fn line_break(&mut self) -> Result<()> {
+        writeln!(self.w).map_err(From::from)
+    }
+
+    fn line(&mut self, f: &mut FnMut(&mut Write) -> Result<()>) -> Result<()> {
+        let mut buf = Vec::new();
+        f(&mut buf)?;
+        if !buf.is_empty() {
+            match self.prefix {
+                DiffPrefix::None => {}
+                DiffPrefix::Equal => write!(self.w, "  ")?,
+                DiffPrefix::Less => {
+                    write!(self.w, "- ")?;
+                }
+                DiffPrefix::Greater => {
+                    write!(self.w, "+ ")?;
+                }
+            }
+            for _ in 0..self.indent {
+                write!(self.w, "\t")?;
+            }
+            self.w.write_all(&*buf)?;
+            writeln!(self.w)?;
+        }
+        Ok(())
+    }
+
+    fn indent_begin(&mut self) {
+        self.indent += 1;
+    }
+
+    fn indent_end(&mut self) {
+        self.indent -= 1;
+    }
+
+    fn prefix(&mut self, prefix: DiffPrefix) {
+        self.prefix = prefix;
+    }
+
+    fn inline_begin(&mut self) -> bool {
+        if self.inline_depth == 0 {
+            false
+        } else {
+            self.inline_depth -= 1;
+            true
+        }
+    }
+
+    fn inline_end(&mut self) {
+        self.inline_depth += 1;
+    }
+}
