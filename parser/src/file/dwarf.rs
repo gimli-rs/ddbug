b557f6d249518bf36a1cccf7f10500509cf8add1
Philip Craig <philipjcraig@gmail.com>
2025-09-14 18:03:20 +1000

    Fix compiler warnings

--- parser/src/file/dwarf.rs
+++ parser/src/file/dwarf.rs
@@ -228,17 +228,17 @@ impl<'a, R: gimli::Reader<Offset = usize>> gimli::Reader for Relocate<'a, R> {
     }
 
     #[inline]
-    fn to_slice(&self) -> gimli::Result<Cow<[u8]>> {
+    fn to_slice(&self) -> gimli::Result<Cow<'_, [u8]>> {
         self.reader.to_slice()
     }
 
     #[inline]
-    fn to_string(&self) -> gimli::Result<Cow<str>> {
+    fn to_string(&self) -> gimli::Result<Cow<'_, str>> {
         self.reader.to_string()
     }
 
     #[inline]
-    fn to_string_lossy(&self) -> gimli::Result<Cow<str>> {
+    fn to_string_lossy(&self) -> gimli::Result<Cow<'_, str>> {
         self.reader.to_string_lossy()
     }
 
@@ -308,7 +308,7 @@ where
         offset: gimli::DebugInfoOffset,
     ) -> Option<(
         &DwarfUnit<'input, Endian>,
-        gimli::EntriesTree<Reader<'input, Endian>>,
+        gimli::EntriesTree<'_, '_, Reader<'input, Endian>>,
     )> {
         // FIXME: make this more efficient for large numbers of units
         // FIXME: cache lookups
@@ -323,11 +323,11 @@ where
     }
 
     fn type_tree(
-        &self,
+        &'_ self,
         offset: TypeOffset,
     ) -> Option<(
         &DwarfUnit<'input, Endian>,
-        gimli::EntriesTree<Reader<'input, Endian>>,
+        gimli::EntriesTree<'_, '_, Reader<'input, Endian>>,
     )> {
         offset
             .get()
@@ -335,11 +335,11 @@ where
     }
 
     fn function_tree(
-        &self,
+        &'_ self,
         offset: FunctionOffset,
     ) -> Option<(
-        &DwarfUnit<'input, Endian>,
-        gimli::EntriesTree<Reader<'input, Endian>>,
+        &'_ DwarfUnit<'input, Endian>,
+        gimli::EntriesTree<'_, '_, Reader<'input, Endian>>,
     )> {
         offset
             .get()
